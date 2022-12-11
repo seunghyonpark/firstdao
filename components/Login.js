813c6995bdf0df4049ba50d39875e97b682f4b83
seunghyonpark <creath.park@gmail.com>
2022-12-11 15:39:13 +0900

    login state update

--- components/Login.js
+++ components/Login.js
@@ -4,16 +4,11 @@ import styles from "../styles/Login.module.css"
 const Login = () => {
   const connectWithMetamask = useMetamask()
   return (
-    <div>
-      <button onClick={connectWithMetamask}>Sign in using MetaMask</button>
+    <div className={styles.container}>
+        <button className={styles.button} onClick={connectWithMetamask}>
+            Sign in using MetaMask
+        </button>
     </div>
   )
 }
 export default Login;
-
-
-<div className={styles.container}>
-  <button className={styles.button} onClick={() => connectWallet("injected")}>
-    Sign in using MetaMask
-  </button>
-</div>
