1474295661fc3ba409722f979fefccb6b41b266d
seunghyonpark <creath.park@gmail.com>
2022-12-11 18:28:45 +0900

    add mint-token script

--- scripts/initialize-sdk.js
+++ scripts/initialize-sdk.js
@@ -1,6 +1,6 @@
-import { ThirdwebSDK } from "@thirdweb-dev/sdk";
-import ethers from "ethers";
-import dotenv from "dotenv";
+import { ThirdwebSDK } from "@thirdweb-dev/sdk"
+import ethers from "ethers"
+import dotenv from "dotenv"
 
 dotenv.config();
 
